12a0477b2ff135d21fac5b387a794d98730761b9
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-09 14:53:49 +0100

    Added default general to category

--- boxes/models.py
+++ boxes/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='product_images', null=True, blank=True)
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=254, default='General')
     is_available = models.BooleanField(default=True)
 
     def __str__(self):

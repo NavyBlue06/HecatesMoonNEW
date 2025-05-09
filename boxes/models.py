ab7f8c997ff8a6daddea83e70e48f3c74e2cc8d5
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-09 14:43:14 +0100

    Define product model

--- boxes/models.py
+++ boxes/models.py
@@ -8,6 +8,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='product_images', null=True, blank=True)
+    category = models.CharField(max_length=50)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name

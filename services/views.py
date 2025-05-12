01e36c40a215f1038ffb9fbdee7bc12aa64a67ab
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-12 10:32:58 +0100

    Add view to services App // connect it to forms

--- services/views.py
+++ services/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .forms import BirthChartRequestForm
 # Create your views here.
+
+def birth_chart_request(request):
+    if request.method == 'POST':
+        form = BirthChartRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'services/success.html')
+    else:
+        form = BirthChartRequestForm()
+
+    return render(request, 'services/birth_chart_request.html', {'form': form})

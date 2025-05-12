ecb54f409086f5802bc1b37d34a112b5f36077af
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-12 17:42:06 +0100

    Update the view for the products to split between the items and boxes

--- boxes/views.py
+++ boxes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from .models import Product
 
 def all_products(request):
-    return render(request, 'boxes/all_products.html')  # make sure this template exists
+    products = Product.objects.filter(is_available=True)
+    boxes = products.filter(category__icontains='box')
+    items = products.exclude(category__icontains='box')
+    
+    return render(request, 'boxes/all_products.html', {
+        'boxes': boxes,
+        'items': items,
+    })

843b9ad6611ea17f587594f624bcb08f0e72909d
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-12 17:41:22 +0100

    Update Models boxes

--- boxes/models.py
+++ boxes/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
+
 
 class Product(models.Model):
     name = models.CharField(max_length=254)
@@ -13,3 +12,5 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    

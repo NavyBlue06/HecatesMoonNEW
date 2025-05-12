54f18891cc4613d410d6d7ec0f83e4f5a3aba2dd
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-12 11:01:49 +0100

    Add forms for the rest of the services

--- services/forms.py
+++ services/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import BirthChartRequest
+from .models import BirthChartRequest, WitchQuestion, RitualRequest, DreamSubmission, MediumContactRequest
+
 
 class BirthChartRequestForm(forms.ModelForm):
     class Meta:
@@ -11,3 +12,23 @@ class BirthChartRequestForm(forms.ModelForm):
             'birth_date': forms.DateInput(attrs={'type': 'date'}),
             'birth_time': forms.TimeInput(attrs={'type': 'time'}),
         }
+
+class WitchQuestionForm(forms.ModelForm):
+    class Meta:
+        model = WitchQuestion
+        fields = ['full_name', 'email', 'question']
+
+class RitualRequestForm(forms.ModelForm):
+    class Meta:
+        model = RitualRequest
+        fields = ['full_name', 'email', 'intention', 'details', 'urgency']
+
+class DreamSubmissionForm(forms.ModelForm):
+    class Meta:
+        model = DreamSubmission
+        fields = ['full_name', 'email', 'dream_description', 'recurring']
+
+class MediumContactForm(forms.ModelForm):
+    class Meta:
+        model = MediumContactRequest
+        fields = ['full_name', 'email', 'message', 'focus_area']

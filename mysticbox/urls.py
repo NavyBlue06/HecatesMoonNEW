8f79a127c9ef9c65bccf4f093951212975cda2b8
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-14 21:05:57 +0100

    Correct indentation

--- mysticbox/urls.py
+++ mysticbox/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
 ]
 # Serve static and media files in development
 if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

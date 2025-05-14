eaf8e7a2d6ec4bb696cdbe8475477f8f456cef2a
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-14 21:42:11 +0100

    Update path to the Url in nav 'services'

--- services/urls.py
+++ services/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.services_home, name='services'),
+
     path('birth-chart/', views.birth_chart_request, name='birth_chart'),
 
-    # Future routes for your other services:
+    
     path('ask-a-witch/', views.ask_a_witch, name='ask_a_witch'),
     path('ritual-request/', views.ritual_request, name='ritual_request'),
     path('dream-interpretation/', views.dream_interpretation, name='dream_interpretation'),

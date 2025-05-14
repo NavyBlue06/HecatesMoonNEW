c255fdf6e24e5f394a7e7216e628ed938e6bc978
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-14 18:33:50 +0100

    Add static to the paths

--- mysticbox/urls.py
+++ mysticbox/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +26,7 @@ urlpatterns = [
     path('boxes/', include('boxes.urls')),  # Include boxes app URLs
     path('cart/', include('cart.urls')),  # Include cart app URLs
     path('services/', include('services.urls')),
-
 ]
+# Serve static and media files in development
+if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

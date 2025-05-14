229511171d1929e63e48cc8f69e023f3fc6e153a
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-14 14:15:23 +0100

    Add services to admin

--- services/admin.py
+++ services/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import BirthChartRequest, RitualBooking, WitchQuestion, DreamInterpretation
+from .models import BirthChartRequest, RitualRequest, WitchQuestion, DreamSubmission, MediumContactRequest
 
 admin.site.register(BirthChartRequest)
-admin.site.register(RitualBooking)
+admin.site.register(RitualRequest)
 admin.site.register(WitchQuestion)
-admin.site.register(DreamInterpretation)
+admin.site.register(DreamSubmission)
+admin.site.register(MediumContactRequest)
 

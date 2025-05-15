79c982229162eb2820511be6bd14ff4327685f04
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-15 15:39:36 +0100

    Add market_items view to display MagicalItem objects

--- hecatemarket/views.py
+++ hecatemarket/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from boxes.models import Product
+from boxes.models import Product  # correct name of model Navahlicious!
 
-# Create your views here.
 def market_landing(request):
     return render(request, 'hecatemarket/market_landing.html')
 
 def market_items(request):
-    items = Product.objects.filter(is_available=True).exclude(category__icontains='box')
+    items = Product.objects.filter(is_available=True)
     return render(request, 'hecatemarket/market_items.html', {'items': items})

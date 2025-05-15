6ad63ce5ab5a8fee56158224624d522cf6a8dbd0
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-15 09:29:09 +0100

    Create add_to_cart view

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,15 @@
+from django.shortcuts import redirect, get_object_or_404
+from boxes.models import Product
 from django.shortcuts import render
 from .cart import Cart
 
 def cart_detail(request):
     cart = Cart(request)
     return render(request, 'cart/cart_detail.html', {'cart': cart})
+
+# add to cart
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product)
+    return redirect('cart')

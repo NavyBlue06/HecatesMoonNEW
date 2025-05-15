15292dda09ab6ad0b917ce744e12a0d819e3f821
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-15 09:56:21 +0100

    Add market item view and urls

--- hecatemarket/urls.py
+++ hecatemarket/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.market_landing, name='market_landing'),
+    path('items/', views.market_items, name='market_items'), # magical items
 ]
--- hecatemarket/views.py
+++ hecatemarket/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from boxes.models import Product
 
 # Create your views here.
 def market_landing(request):
     return render(request, 'hecatemarket/market_landing.html')
+
+def market_items(request):
+    items = Product.objects.filter(is_available=True).exclude(category__icontains='box')
+    return render(request, 'hecatemarket/market_items.html', {'items': items})

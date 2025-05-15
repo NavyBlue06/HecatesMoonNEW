ac6d43f06cba58c4c96770e8ec002bf565ea36da
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-15 17:04:19 +0100

    Modify the magical items in views, Got test error, boxes started loading to magical items

--- hecatemarket/views.py
+++ hecatemarket/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from boxes.models import Product  # correct name of model Navahlicious!
+from hecatemarket.models import MagicalItem # correct name of model Navahlicious!
 
 def market_landing(request):
     return render(request, 'hecatemarket/market_landing.html')
 
 def market_items(request):
-    items = Product.objects.filter(is_available=True)
+    items = MagicalItem.objects.filter(is_available=True)
     return render(request, 'hecatemarket/market_items.html', {'items': items})

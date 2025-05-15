acf05f6168ad56e5e92d6fad0748ac893f100f1f
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-15 10:23:06 +0100

    Make sure context processor exist and comment it

--- mysticbox/settings.py
+++ mysticbox/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.context_processors.cart_context',
+                'cart.context_processors.cart_context', # Custom context processor for cart
 
             ],
         },

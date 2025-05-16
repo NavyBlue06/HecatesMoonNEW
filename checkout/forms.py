38d99cb6a48cf3010eaf22db8dffacc437a9cc11
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-16 16:54:01 +0100

    Implement checkout admin, view, order form, and checkout template for order processing

---
+++ checkout/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = (
+            'full_name', 'email', 'phone_number',
+            'address1', 'address2', 'city', 'postcode', 'country', 'county',
+        )
+
+        widgets = {
+            'country': forms.Select(attrs={'class': 'form-select'}),
+        }
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import OrderForm
+from .models import Order
+from cart.cart import Cart
 
-# Create your views here.
+def checkout(request):
+    cart = Cart(request)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.save()
+            # I’ll add line items + stripe here soon
+            return redirect('checkout_success', order_number=order.order_number)
+    else:
+        form = OrderForm()
+
+    context = {
+        'form': form,
+        'cart': cart,
+    }
+
+    return render(request, 'checkout/checkout.html', context)
+
